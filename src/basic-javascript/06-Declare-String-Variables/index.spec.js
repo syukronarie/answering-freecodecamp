eb6e5d0c93fe18e4971ba6a6dc1ab65e615b4dce
syukronarie <syukronarie@gmail.com>
2022-05-06 22:41:14 +0700

    declare string variables

---
+++ src/basic-javascript/06-Declare-String-Variables/index.spec.js
@@ -0,0 +1,76 @@
+/**
+ * Declare-String-Variables
+ * Todo: Create two new string variables: `myFirstName` and `myLastName` and assign them the values of your first and last name, respectively.
+ * * Hint:
+ * * `myFirstName` should be a string with at least one character in it.
+ * * `myLastName` should be a string with at least one character in it.
+ */
+
+// write your code below
+
+var myFirstName = 'Arie';
+var myLastName = 'Syukron';
+
+// _________ _______  _______ _________   _______  _______  _______  _______  _______
+// \__   __/(  ____ \(  ____ \\__   __/  (  ____ \(  ___  )(  ____ \(  ____ \(  ____ \
+//    ) (   | (    \/| (    \/   ) (     | (    \/| (   ) || (    \/| (    \/| (    \/
+//    | |   | (__    | (_____    | |     | |      | (___) || (_____ | (__    | (_____
+//    | |   |  __)   (_____  )   | |     | |      |  ___  |(_____  )|  __)   (_____  )
+//    | |   | (            ) |   | |     | |      | (   ) |      ) || (            ) |
+//    | |   | (____/\/\____) |   | |     | (____/\| )   ( |/\____) || (____/\/\____) |
+//    )_(   (_______/\_______)   )_(     (_______/|/     \|\_______)(_______/\_______)
+//                             ____       _
+//                             |  _ \     | |
+//                             | |_) | ___| | _____      __
+//                             |  _ < / _ \ |/ _ \ \ /\ / /
+//                             | |_) |  __/ | (_) \ V  V /
+//                             |____/ \___|_|\___/ \_/\_/
+//                         ______ ______ ______ ______ ______
+//                         |______|______|______|______|______|
+
+//                          ______ ______ ______ ______ ______
+//                         |______|______|______|______|______|
+
+//                          ______ ______ ______ ______ ______
+//                         |______|______|______|______|______|
+
+if (typeof myFirstName !== 'undefined' && typeof myLastName !== 'undefined') {
+  (function () {
+    return myFirstName + ', ' + myLastName;
+  })();
+}
+
+const { assert } = require('chai');
+
+describe('Declare String Variables', () => {
+  it('`myFirstName` should be a string with at least one character in it.', () => {
+    assert(
+      (function () {
+        if (
+          typeof myFirstName !== 'undefined' &&
+          typeof myFirstName === 'string' &&
+          myFirstName.length > 0
+        ) {
+          return true;
+        } else {
+          return false;
+        }
+      })()
+    );
+  });
+  it('`myLastName` should be a string with at least one character in it.', () => {
+    assert(
+      (function () {
+        if (
+          typeof myLastName !== 'undefined' &&
+          typeof myLastName === 'string' &&
+          myLastName.length > 0
+        ) {
+          return true;
+        } else {
+          return false;
+        }
+      })()
+    );
+  });
+});
---
+++ src/basic-javascript/06-Declare-String-Variables/readme.md
@@ -0,0 +1,21 @@
+## Declare-String-Variables
+
+Previously you used the following code to declare a variable:
+
+```js
+var myName;
+```
+
+But you can also declare a string variable like this:
+
+```js
+var myName = 'your name';
+```
+
+`"your name"` is called a <dfn>string</dfn> <dfn>literal</dfn>. A string literal, or string, is a series of zero or more characters enclosed in single or double quotes.
+
+---
+
+_Task:_ Create two new string variables: `myFirstName` and `myLastName` and assign them the values of your first and last name, respectively.
+
+---
