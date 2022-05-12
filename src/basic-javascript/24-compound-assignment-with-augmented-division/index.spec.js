b4c569ee71f89ce27d57a3cdc595ec05794ce657
syukronarie <syukronarie@gmail.com>
2022-05-12 12:51:13 +0700

    passed: compound assignment with augmented division

---
+++ src/basic-javascript/24-compound-assignment-with-augmented-division/index.spec.js
@@ -0,0 +1,75 @@
+/**
+ * Compound Assignment With Augmented Division
+ * Todo: Convert the assignments for `a`, `b`, and `c` to use the `/=` operator.
+ * * The variable `product` should be equal to 80.
+ * * You should use the `*` operator.
+ */
+
+function writeYourCode() {
+  // write your code below
+
+  let a = 48;
+  let b = 108;
+  let c = 33;
+
+  // Only change code below this line
+  a /= 12;
+  b /= 4;
+  c /= 11;
+
+  return {
+    a,
+    b,
+    c,
+  };
+}
+
+// _________ _______  _______ _________   _______  _______  _______  _______  _______
+// \__   __/(  ____ \(  ____ \\__   __/  (  ____ \(  ___  )(  ____ \(  ____ \(  ____ \
+//    ) (   | (    \/| (    \/   ) (     | (    \/| (   ) || (    \/| (    \/| (    \/
+//    | |   | (__    | (_____    | |     | |      | (___) || (_____ | (__    | (_____
+//    | |   |  __)   (_____  )   | |     | |      |  ___  |(_____  )|  __)   (_____  )
+//    | |   | (            ) |   | |     | |      | (   ) |      ) || (            ) |
+//    | |   | (____/\/\____) |   | |     | (____/\| )   ( |/\____) || (____/\/\____) |
+//    )_(   (_______/\_______)   )_(     (_______/|/     \|\_______)(_______/\_______)
+//                             ____       _
+//                             |  _ \     | |
+//                             | |_) | ___| | _____      __
+//                             |  _ < / _ \ |/ _ \ \ /\ / /
+//                             | |_) |  __/ | (_) \ V  V /
+//                             |____/ \___|_|\___/ \_/\_/
+//                         ______ ______ ______ ______ ______
+//                         |______|______|______|______|______|
+
+//                          ______ ______ ______ ______ ______
+//                         |______|______|______|______|______|
+
+//                          ______ ______ ______ ______ ______
+//                         |______|______|______|______|______|
+
+const { assert } = require('chai');
+
+const code = writeYourCode.toString();
+const { a, b, c } = writeYourCode();
+
+describe('24 - Compound Assignment With Augmented Division', () => {
+  it('`a` should equal `4`.', () => {
+    assert(a === 4);
+  });
+  it('`b` should equal `27`.', () => {
+    assert(b === 27);
+  });
+  it('`c` should equal `3`.', () => {
+    assert(c === 3);
+  });
+  it('You should use the `/=` operator for each variable.', () => {
+    assert(code.match(/\/=/g).length === 3);
+  });
+  it('You should not modify the code above the specified comment.', () => {
+    assert(
+      /let a = 48;/.test(code) &&
+        /let b = 108;/.test(code) &&
+        /let c = 33;/.test(code)
+    );
+  });
+});
