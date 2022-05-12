20ebe3a3dc1acfd2e66d384bfefd94158b553b79
syukronarie <syukronarie@gmail.com>
2022-05-12 10:40:55 +0700

    pass: devide one number by another

---
+++ src/basic-javascript/14-divide-one-number-by-another-with-javascript/index.spec.js
@@ -0,0 +1,55 @@
+const __helpers = require('../../helpers');
+
+/**
+ * Divide One Number by Another with JavaScript
+ * Todo: Change the `0` so that the `quotient` is equal to `2`.
+ * * The variable `quotient` should be equal to 2.
+ * * You should use the `/` operator.
+ */
+
+function writeYourCode() {
+  // write your code below
+
+  const quotient = 66 / 33;
+
+  return {
+    quotient,
+  };
+}
+
+// _________ _______  _______ _________   _______  _______  _______  _______  _______
+// \__   __/(  ____ \(  ____ \\__   __/  (  ____ \(  ___  )(  ____ \(  ____ \(  ____ \
+//    ) (   | (    \/| (    \/   ) (     | (    \/| (   ) || (    \/| (    \/| (    \/
+//    | |   | (__    | (_____    | |     | |      | (___) || (_____ | (__    | (_____
+//    | |   |  __)   (_____  )   | |     | |      |  ___  |(_____  )|  __)   (_____  )
+//    | |   | (            ) |   | |     | |      | (   ) |      ) || (            ) |
+//    | |   | (____/\/\____) |   | |     | (____/\| )   ( |/\____) || (____/\/\____) |
+//    )_(   (_______/\_______)   )_(     (_______/|/     \|\_______)(_______/\_______)
+//                             ____       _
+//                             |  _ \     | |
+//                             | |_) | ___| | _____      __
+//                             |  _ < / _ \ |/ _ \ \ /\ / /
+//                             | |_) |  __/ | (_) \ V  V /
+//                             |____/ \___|_|\___/ \_/\_/
+//                         ______ ______ ______ ______ ______
+//                         |______|______|______|______|______|
+
+//                          ______ ______ ______ ______ ______
+//                         |______|______|______|______|______|
+
+//                          ______ ______ ______ ______ ______
+//                         |______|______|______|______|______|
+
+const { assert } = require('chai');
+
+const code = writeYourCode.toString();
+const { quotient } = writeYourCode();
+
+describe('14 - Divide One Number by Another with JavaScript', () => {
+  it('The variable `quotient` should be equal to 2.', () => {
+    assert(quotient === 2);
+  });
+  it('You should use the `/` operator.', async () => {
+    assert(/\//.test(code));
+  });
+});
